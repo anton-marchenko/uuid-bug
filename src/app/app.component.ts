a14f433ea8b20c21274bc7ecff67ae63c48f11fc
anton-marchenko <fattan@ya.ru>
2020-08-13 18:13:08 +0300

    Emulate bad scenarious
    22

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'uuid-test';
+
+  ngOnInit() {
+    console.log('uuidv4()', uuidv4());
+    this.title = uuidv4();
+  }
 }
